9254fcb2c6082380a098332d6222f19974fa1092
ChenNing02 <2633565885@qq.com>
2020-07-25 14:55:44 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -117,208 +117,74 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"test.js":[function(require,module,exports) {
-"use strict";
+})({"C:/Users/18348/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-var _default = [{
-  title: '前置问题',
-  text: ['简单的做个自我介绍', '简单的介绍一下你的项目', '之前的工作主要做一些什么']
-}, {
-  title: 'HTML押题',
-  random: 2,
-  text: ['你是如何理解HTML语义化的？（举例）', 'meta viewport 是做什么的，怎么写？（举例）', '你用过哪些HTML5标签（举例）', 'H5是什么（概念）']
-}, {
-  title: 'CSS押题',
-  random: 2,
-  text: ['两种盒模型分别说一下。', '如何垂直居中？', 'flex 怎么用，常用属性有哪些？', 'BFC 是什么？', 'CSS 选择器优先级？', '如何清除浮动？']
-}, {
-  title: 'JS押题',
-  random: 5,
-  text: ['ES 6 语法知道哪些，分别怎么用？', 'Promise、Promise.all、Promise.race 分别怎么用？', '手写函数防抖和函数节流', '手写AJAX', '这段代码里的 this 是什么？', '闭包/立即执行函数是什么？', '什么是 JSONP，什么是 CORS，什么是跨域？', 'async/await 怎么用，如何捕获异常？', '如何实现深拷贝？', '如何用正则实现 trim()？', '不用 class 如何实现继承？用 class 又如何实现？', '如何实现数组去重？', '手写一个 Promise']
-}, {
-  title: 'Vue押题',
-  random: 2,
-  text: ['watch 和 computed 和 methods 区别是什么？', 'Vue 有哪些生命周期钩子函数？分别有什么用？', 'Vue 如何实现组件间通信？', 'Vue 数据响应式怎么做到的？（双向绑定的原理）', 'Vue.set 是做什么用的？', 'Vuex 的用法？', 'VueRouter 的用法？', '路由守卫是什么？']
-}, {
-  title: '开放性问题',
-  random: 2,
-  text: ['说说你对行业、技术发展趋势的看法。', '说说你在工作中遇到的最难的问题，以及你是如何解决该问题的。', '为什么从上一家公司离职？', '你最近是否参加了培训课程。', '你还有什么问题要问的吗。']
-}];
-exports.default = _default;
-},{}],"main.js":[function(require,module,exports) {
-"use strict";
-
-var _test = _interopRequireDefault(require("./test.js"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-var unfilteredData = _test.default; // 未筛选的数据
-
-var reset = get_el('#resetContent');
-var app = get_el('#app');
-var pdf = get_el('#printPDF');
-var localKey = 'uphgQuestion'; // localStorage 数据存储的名称
-
-initElement(app, {
-  key: localKey,
-  value: unfilteredData
-}); // 重置按钮
-
-reset.onclick = function () {
-  // 获取随机筛选后的数据
-  var QuestionList = addChapter(unfilteredData); // 将它添加到 dom 中
-
-  loadTemplate(app, QuestionList); // 将它存储到 local 中
-
-  setLocal(localKey, QuestionList);
-}; // PDF打印按钮
-
-
-pdf.onclick = function () {
-  window.print();
-}; // 随机筛选数据
-
-
-function addChapter(data) {
-  var isData = [];
-
-  if (type(data) === 'array' || type(data) === 'object') {
-    isData = Array.from(data);
-  } else {
-    console.error('请传入一个「数组」或「伪数组」');
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
   }
 
-  var title = ''; // problemList：要展示的问题，getQuestionList：要展示的所有数据
-
-  var problemList = [],
-      getQuestionList = [];
-  isData.forEach(function (item, index) {
-    // 清空之前的内容
-    title = item.title; //随机抽取指定数量的题目
-
-    if (!!item.random) {
-      var number = parseInt(item.random);
-      problemList = randomArray(item.text, number).result;
-      getQuestionList[index] = {
-        title: title,
-        text: _toConsumableArray(problemList)
-      };
-    } else {
-      problemList = item.text;
-      getQuestionList[index] = {
-        title: title,
-        text: _toConsumableArray(problemList)
-      };
-    }
-  });
-  return getQuestionList; // 返回当前随机的后的问题
-} // 第一次加载数据
-
-
-function initElement(element) {
-  var local = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-  var data = {};
-
-  if (asserLocal(local.key)) {
-    data = getLocal(local.key);
-  } else {
-    setLocal(local.key, local.value);
-    data = local.value;
-  }
-
-  loadTemplate(element, data);
-} // 在指定元素中加载模板
-
-
-function loadTemplate(element, data) {
-  var parent = '',
-      template = '',
-      ulContent = '',
-      title = '';
-  var list = [];
-  data.forEach(function (item) {
-    ulContent = '';
-    title = item.title;
-    list = item.text;
-    list.forEach(function (text) {
-      ulContent += "<li>".concat(text, "</li>");
-    }); // 将抽取后的题目和大标题放入模板中
-
-    template = "<section class=\"part\"><h2>".concat(title, "</h2><ol>").concat(ulContent, "</ol></section>"); // 将模板添加到一个div中
-
-    parent += template;
-  });
-  element.innerHTML = parent;
-  return parent;
-} // 获取元素
-
-
-function get_el(element) {
-  return document.querySelector(element);
-} // 随机抽取题目
-
-
-function randomArray(array, number) {
-  var isArray = Array.from(array);
-  var result = [];
-
-  for (var i = 0; i < number; i++) {
-    var single = Math.floor(Math.random() * isArray.length);
-    result.push(isArray.splice(single, 1)[0]);
-  }
-
-  return {
-    result: result,
-    // 随机抽取后的数组
-    after: isArray // 原数组的拷贝
-
-  };
-} // 将字符串格式的HTML转换为DOM元素
-// function changeLabel(element){
-//     let div = document.createElement('div')
-//     div.innerHTML = element
-//     return div.firstElementChild
-// }
-
-
-function asserLocal(name) {
-  return localStorage.hasOwnProperty(name);
-} // 根据存储名称获取 localStorage 的值
-
-
-function getLocal(name) {
-  return JSON.parse(localStorage.getItem(name));
-} // 设置/存储 localStorage 的值
-
-
-function setLocal(name, data) {
-  localStorage.setItem(name, JSON.stringify(data));
-} // 判断数据类型
-
-
-function type(o) {
-  var s = Object.prototype.toString.call(o);
-  return s.match(/\[object (.*?)\]/)[1].toLowerCase();
+  return bundleURL;
 }
 
-;
-},{"./test.js":"test.js"}],"C:/Users/18348/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"C:/Users/18348/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"C:/Users/18348/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"C:/Users/18348/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -522,5 +388,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["C:/Users/18348/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+},{}]},{},["C:/Users/18348/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/index.js.map
--- src/main.js
+++ src/main.js
@@ -1,21 +1,26 @@
-import data from './test.js'
+import testData from './test.js'
 
+let unfilteredData = testData     // 未筛选的数据
 let reset = get_el('#resetContent')
 let app = get_el('#app')
 let pdf = get_el('#printPDF')
-
-
-addChapter(data)
-
+let localKey = 'uphgQuestion'     // localStorage 数据存储的名称
+initElement(app, { key: localKey,value: unfilteredData })
 // 重置按钮
 reset.onclick = function () {
-    addChapter(data)
+    // 获取随机筛选后的数据
+    let QuestionList = addChapter(unfilteredData)
+    // 将它添加到 dom 中
+    loadTemplate(app, QuestionList)
+    // 将它存储到 local 中
+    setLocal(localKey, QuestionList)
 }
+// PDF打印按钮
 pdf.onclick = function () {
     window.print() 
 }
 
-// 根据数据随机显示内容
+// 随机筛选数据
 function addChapter(data) {
     let isData = []
     if (type(data) === 'array' || type(data) === 'object') {
@@ -23,17 +28,11 @@ function addChapter(data) {
     } else {
         console.error('请传入一个「数组」或「伪数组」')
     }
-    // let isData = []
-    let numberIsQuestions = []
-    // isData.push(...data)
-    let [parent, template, ulContent, title] = ['', '', '', '']
+    let title = ''
     // problemList：要展示的问题，getQuestionList：要展示的所有数据
     let [problemList, getQuestionList] = [[], []]
-    let ques = numberIsQuestions
-
     isData.forEach((item, index) => {
         // 清空之前的内容
-        ulContent = ''
         title = item.title
         //随机抽取指定数量的题目
         if (!!item.random) {
@@ -50,23 +49,39 @@ function addChapter(data) {
                 text: [...problemList]
             }
         }
-        problemList.forEach(text => {
+    })
+    return getQuestionList // 返回当前随机的后的问题
+}
+// 第一次加载数据
+function initElement(element, local = {}){
+    let data = {}
+    if(asserLocal(local.key)){
+        data = getLocal(local.key)
+    }else{
+        setLocal(local.key, local.value)
+        data = local.value
+    }
+    loadTemplate(element, data)
+}
+// 在指定元素中加载模板
+function loadTemplate(element,data){
+    let [parent, template, ulContent, title] = ['', '', '', '']
+    let list = []
+    data.forEach((item) => {
+        ulContent = ''
+        title = item.title
+        list = item.text
+        list.forEach(text => {
             ulContent += `<li>${text}</li>`
         })
         // 将抽取后的题目和大标题放入模板中
-        template = `
-        <section class="part">
-            <h2>${title}</h2>
-            <ol>${ulContent}</ol>
-        </section>
-        `
+        template = `<section class="part"><h2>${title}</h2><ol>${ulContent}</ol></section>`
         // 将模板添加到一个div中
         parent += template
     })
-    app.innerHTML = parent
-    return getQuestionList // 返回当前随机的后的问题
+    element.innerHTML = parent
+    return parent
 }
-
 // 获取元素
 function get_el(element) {
     return document.querySelector(element)
@@ -86,22 +101,27 @@ function randomArray(array, number) {
     }
 }
 
-// // 将字符串格式的HTML转换为DOM元素
+// 将字符串格式的HTML转换为DOM元素
 // function changeLabel(element){
 //     let div = document.createElement('div')
 //     div.innerHTML = element
 //     return div.firstElementChild
 // }
 
-// // 根据存储名称获取 localStorage 的值
-// function getLocal(key){
-//     return JSON.parse(localStorage.getItem(key))
-// }
 
-// // 设置/存储 localStorage 的值
-// function setLocal(key, data){
-//     localStorage.setItem(key, JSON.stringify(data))
-// }
+function asserLocal(name){
+    return localStorage.hasOwnProperty(name)
+}
+
+// 根据存储名称获取 localStorage 的值
+function getLocal(name){
+    return JSON.parse(localStorage.getItem(name))
+}
+
+// 设置/存储 localStorage 的值
+function setLocal(name, data){
+    localStorage.setItem(name, JSON.stringify(data))
+}
 
 // 判断数据类型
 function type(o) {
